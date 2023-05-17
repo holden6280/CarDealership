241df44a858da706b4e29d4f8a796eb76e5bc129
holden6280 <emiicleghorn@gmail.com>
2023-05-17 17:49:35 -0400

    Added a lot. Will continue later.

--- src/main/java/com/cardealership/DealershipFileManager.java
+++ src/main/java/com/cardealership/DealershipFileManager.java
@@ -5,14 +5,7 @@ import java.io.FileReader;
 
 public class DealershipFileManager {
     public Dealership getDealership() {
-        Dealership dealership = new Dealership("Esserman International", "10455 NW 12th St, Doral, FL 33172", "(305) 477-6666");
 
-        try{
-            FileReader fileReader = new FileReader("EssermanInternational.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-
-        }
     }
 
     public Dealership saveDealership() {
--- src/main/java/com/cardealership/UserInterface.java
+++ src/main/java/com/cardealership/UserInterface.java
@@ -1,6 +1,17 @@
 package com.cardealership;
 
+import java.util.*;
+
 public class UserInterface {
+
+    private Dealership dealership;
+    Scanner scanner;
+    private void init() {
+        return ArrayList<dealership>;
+    }
+    public display() {
+
+    }
 
     //dealership : Dealership
 
